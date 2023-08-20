3503bbbe03a818a4bd88e7975e50b2c69470f680
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-08-20 17:53:31 +0300

    add pckgs

--- main.ts
+++ main.ts
@@ -516,3 +516,43 @@ const carRecord: TypeRecord = { // по примеру понятно
     name: 11,
     price: '33',
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Декораторы.
+function LogClass(constructor: Function) {
+    console.log(constructor.name);
+}
+
+function LogMethod(target: Object, key: string, descriptor: PropertyDescriptor) {
+    console.log(key);
+}
+
+
+@LogClass
+class Plane {
+    private id: number
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    @LogMethod
+    getId() {
+        return this.id;
+    }
+}
